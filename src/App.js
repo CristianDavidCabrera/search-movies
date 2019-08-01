cb9d79d71dc37acbafb9e22a0d765726b04fa645
Cristian David Cabrera Cabrera <cristiancabrera.1985@gmail.com>
2019-08-01 18:56:52 +0100

    Created MovieList an Movie components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Title } from './components/Title';
 import { SearchForm } from './components/SearchForm';
+import { MovieList } from './components/MoviesLists';
 
 import './App.css';
 import 'bulma/css/bulma.css';
@@ -12,13 +13,6 @@ class App extends Component {
     this.setState({ results })
   }
 
-  _renderResults() {
-    const { results } = this.state
-    return results.map( movie => {
-      return <p key={movie.imdbID}>{movie.Title}</p>
-    })
-  }
-
   render() {
     return (
       <div className="App">
@@ -28,7 +22,7 @@ class App extends Component {
         </div>
         {this.state.results.length === 0
           ? <p>Sin resultados</p>
-          : this._renderResults()
+          : <MovieList movies = {this.state.results} />
         }
       </div>
     ); 
