a838ebae71bd7014205d63da69fcdeb17307ba9e
Cristian David Cabrera Cabrera <cristiancabrera.1985@gmail.com>
2019-08-01 17:01:34 +0100

    SearchForm component: state control done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Title} from './components/Title';
+import { Title } from './components/Title';
+import { SearchForm } from './components/SearchForm';
 import './App.css';
 import 'bulma/css/bulma.css';
 
@@ -7,8 +8,11 @@ function App() {
   return (
     <div className="App">
       <Title>Search Movies</Title>
+      <div className="SearchForm-wrapper">
+        <SearchForm/>
+      </div>
     </div>
-  );
+  ); 
 }
 
 export default App;
