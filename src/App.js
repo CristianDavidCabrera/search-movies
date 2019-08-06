3a5e011f37e073810cfcaec0cb7453fcf733e286
Cristian David Cabrera Cabrera <cristiancabrera.1985@gmail.com>
2019-08-06 12:05:06 +0100

    Detail pages added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Title } from './components/Title';
 import { SearchForm } from './components/SearchForm';
 import { MovieList } from './components/MoviesLists';
+import { Detail } from './pages/Detail'
 
 import './App.css';
 import 'bulma/css/bulma.css';
@@ -19,6 +20,8 @@ class App extends Component {
   }
 
   render() {
+
+
     return (
       <div className="App">
         <Title>Search Movies</Title>
